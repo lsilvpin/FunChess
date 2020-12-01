3222cc7fd283a96b29473a981926559f424e2f50
lsilvpin <lsilvpin@everis.com>
2020-12-01 00:55:55 -0300

    [RulesForMovingBishops] Implementei mais um teste unitário para testar o caso em que o bispo está bloqueado por uma peça aliada.

--- FunChess.XUnitTests/CoreTests/Models/Pieces/BishopTests.cs
+++ FunChess.XUnitTests/CoreTests/Models/Pieces/BishopTests.cs
@@ -20,6 +20,31 @@ namespace FunChess.XUnitTests.CoreTests.Models.Pieces
         }
 
         [Fact]
+        public void IsAllowedSetBeenCalculatedCorrectlyWhenBishopIsBlocked()
+        {
+            // Arrange
+            Board board = core.CreateEmptyBoard();
+            Bishop bishop = core.CreateBishop(PieceColor.White);
+            Pawn pawn = core.CreatePawn(PieceColor.White);
+            Position bishopPosition = core.CreatePosition(1, 1);
+            Position pawnPosition = core.CreatePosition(3, 3);
+
+            board.PutAt(pawn, pawnPosition);
+            board.PutAt(bishop, bishopPosition);
+
+            // Act
+            bool[,] bishopPermissionMatrix = bishop.GetPermissionMatrix(board);
+            int amountOfPermitedPositions = bishop.CountPermitedPositions();
+            HashSet<Position> bishopAllowedSet = bishop.GetAllowedSet();
+
+            // Assert
+            Assert.NotNull(bishopPermissionMatrix);
+            Assert.Equal(4, amountOfPermitedPositions);
+            Assert.NotNull(bishopAllowedSet);
+            PrvAssertPositionsIsOkInCaseBishopIsBlocked(bishopAllowedSet);
+        }
+
+        [Fact]
         public void IsPermissionBeenCalculatedCorrectlyWhenBishopIsCompletlyFreeToMove()
         {
             // Arrange
@@ -38,11 +63,19 @@ namespace FunChess.XUnitTests.CoreTests.Models.Pieces
             Assert.Equal(13, amountOfPermitedPositions);
             Assert.NotNull(bishopAllowedSet);
             Assert.Equal(13, bishopAllowedSet.Count);
-            PrvAssertPositionsInAllowedSetAreOk(bishopAllowedSet);
+            PrvAssertPositionsIsOkInCaseBishopIsFree(bishopAllowedSet);
         }
 
         #region Private helpers
-        private void PrvAssertPositionsInAllowedSetAreOk(HashSet<Position> bishopAllowedSet)
+        private void PrvAssertPositionsIsOkInCaseBishopIsBlocked(HashSet<Position> bishopAllowedSet)
+        {
+            Assert.Contains(core.CreatePosition(0, 0), bishopAllowedSet);
+            Assert.Contains(core.CreatePosition(2, 2), bishopAllowedSet);
+            Assert.Contains(core.CreatePosition(2, 0), bishopAllowedSet);
+            Assert.Contains(core.CreatePosition(0, 2), bishopAllowedSet);
+        }
+
+        private void PrvAssertPositionsIsOkInCaseBishopIsFree(HashSet<Position> bishopAllowedSet)
         {
             Assert.Contains(core.CreatePosition(0, 0), bishopAllowedSet);
             Assert.Contains(core.CreatePosition(1, 1), bishopAllowedSet);
