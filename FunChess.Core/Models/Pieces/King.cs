3dc6c30961d11650b379be0e46b7f111cad4b8de
lsilvpin <lsilvpin@everis.com>
2020-12-04 21:53:44 -0300

    [RuleForMovingKings] Agora os reis sabem se mover pelo tabuleiro respeitando as regras básicas de movimentação.

--- FunChess.Core/Models/Pieces/King.cs
+++ FunChess.Core/Models/Pieces/King.cs
@@ -1,5 +1,6 @@
 ﻿using FunChess.Core.Enums;
 using FunChess.Core.Factory;
+using FunChess.Core.Tools;
 using System;
 
 namespace FunChess.Core.Models.Pieces
@@ -14,7 +15,96 @@ namespace FunChess.Core.Models.Pieces
 
         public override bool[,] GetPermissionMatrix(Board board)
         {
-            throw new NotImplementedException();
+            Position position = core.CreatePosition(Position.Line + 1, Position.Column);
+
+            ValidatePosition(board, position);
+            StepRight(board, position);
+            StepDown(board, position);
+            StepDown(board, position);
+            StepLeft(board, position);
+            StepLeft(board, position);
+            StepUp(board, position);
+            StepUp(board, position);
+
+            return permissionMatrix;
         }
+
+        #region Private helpers
+        private void StepRight(Board board, Position position)
+        {
+            StepRight(position);
+            ValidatePosition(board, position);
+        }
+
+        private void StepRight(Position position)
+        {
+            position.Column = position.Column + 1;
+        }
+
+        private void StepDown(Board board, Position position)
+        {
+            StepDown(position);
+            ValidatePosition(board, position);
+        }
+
+        private void StepDown(Position position)
+        {
+            position.Line = position.Line - 1;
+        }
+
+        private void StepLeft(Board board, Position position)
+        {
+            StepLeft(position);
+            ValidatePosition(board, position);
+        }
+
+        private void StepLeft(Position position)
+        {
+            position.Column = position.Column - 1;
+        }
+
+        private void StepUp(Board board, Position position)
+        {
+            StepUp(position);
+            ValidatePosition(board, position);
+        }
+
+        private void StepUp(Position position)
+        {
+            position.Line = position.Line + 1;
+        }
+
+        private void ValidatePosition(Board board, Position position)
+        {
+            if (Check.IsInsideLimits(position))
+            {
+                Piece piece = board.LookAt(position);
+
+                if (IsEmpty(piece))
+                {
+                    ApprovePosition(position);
+                }
+                else if (IsFoe(piece))
+                {
+                    ApprovePosition(position);
+                }
+            }
+        }
+
+        private bool IsEmpty(Piece piece)
+        {
+            return piece == null;
+        }
+
+        private bool IsFoe(Piece piece)
+        {
+            return piece.Color != Color;
+        }
+
+        private void ApprovePosition(Position position)
+        {
+            permissionMatrix[position.Line, position.Column] = true;
+        }
+        #endregion
     }
 }
--- FunChess.XUnitTests/CoreTests/Models/Pieces/KingTests.cs
+++ FunChess.XUnitTests/CoreTests/Models/Pieces/KingTests.cs
@@ -3,6 +3,7 @@ using FunChess.Core.Factory;
 using FunChess.Core.Models;
 using FunChess.Core.Models.Pieces;
 using System;
+using System.Collections.Generic;
 using System.Linq;
 using Xunit;
 
@@ -17,7 +18,88 @@ namespace FunChess.XUnitTests.CoreTests.Models.Pieces
             core = Beyond.Core;
         }
 
-        
-        
+
+        [Fact]
+        public void IsAllowedSetBeenCalculatedCorrectlyWhenKingIsThreatening()
+        {
+            // Arrange
+            Board board = core.CreateEmptyBoard();
+            King king = core.CreateKing(PieceColor.White);
+            Position kingPosition = core.CreatePosition(3, 4);
+            Knight knight = core.CreateKnight(PieceColor.Black);
+            Position knightPosition = core.CreatePosition(4, 4);
+            board.PutAt(king, kingPosition);
+            board.PutAt(knight, knightPosition);
+
+            // Act
+            bool[,] permissionMatrix = king.GetPermissionMatrix(board);
+            int amountOfPermitedPositions = king.CountPermitedPositions();
+            HashSet<Position> allowedSet = king.GetAllowedSet();
+
+            // Assert
+            Assert.NotNull(permissionMatrix);
+            Assert.Equal(8, amountOfPermitedPositions);
+            PrvAssertAllowedSetIsOkWhenKingIsFreeToMove(allowedSet);
+        }
+
+        [Fact]
+        public void IsAllowedSetBeenCalculatedCorrectlyWhenKingIsBlocked()
+        {
+            // Arrange
+            Board board = core.CreateEmptyBoard();
+            King king = core.CreateKing(PieceColor.White);
+            Position kingPosition = core.CreatePosition(3, 4);
+            Knight knight = core.CreateKnight(PieceColor.White);
+            Position knightPosition = core.CreatePosition(4, 4);
+            board.PutAt(king, kingPosition);
+            board.PutAt(knight, knightPosition);
+
+            // Act
+            bool[,] permissionMatrix = king.GetPermissionMatrix(board);
+            int amountOfPermitedPositions = king.CountPermitedPositions();
+            HashSet<Position> allowedSet = king.GetAllowedSet();
+
+            // Assert
+            Assert.NotNull(permissionMatrix);
+            Assert.Equal(7, amountOfPermitedPositions);
+            PrvAssertAllowedSetIsOkWhenKingIsBlocked(allowedSet);
+        }
+
+        [Fact]
+        public void IsAllowedSetBeenCalculatedCorrectlyWhenKingIsFreeToMove()
+        {
+            // Arrange
+            Board board = core.CreateEmptyBoard();
+            King king = core.CreateKing(PieceColor.White);
+            Position position = core.CreatePosition(3, 4);
+            board.PutAt(king, position);
+
+            // Act
+            bool[,] permissionMatrix = king.GetPermissionMatrix(board);
+            int amountOfPermitedPositions = king.CountPermitedPositions();
+            HashSet<Position> allowedSet = king.GetAllowedSet();
+
+            // Assert
+            Assert.NotNull(permissionMatrix);
+            Assert.Equal(8, amountOfPermitedPositions);
+            PrvAssertAllowedSetIsOkWhenKingIsFreeToMove(allowedSet);
+        }
+
+        private void PrvAssertAllowedSetIsOkWhenKingIsFreeToMove(HashSet<Position> allowedSet)
+        {
+            Assert.Contains(core.CreatePosition(4, 4), allowedSet);
+            PrvAssertAllowedSetIsOkWhenKingIsBlocked(allowedSet);
+        }
+
+        private void PrvAssertAllowedSetIsOkWhenKingIsBlocked(HashSet<Position> allowedSet)
+        {
+            Assert.Contains(core.CreatePosition(4, 5), allowedSet);
+            Assert.Contains(core.CreatePosition(3, 5), allowedSet);
+            Assert.Contains(core.CreatePosition(2, 5), allowedSet);
+            Assert.Contains(core.CreatePosition(2, 4), allowedSet);
+            Assert.Contains(core.CreatePosition(2, 3), allowedSet);
+            Assert.Contains(core.CreatePosition(3, 3), allowedSet);
+            Assert.Contains(core.CreatePosition(4, 3), allowedSet);
+        }
     }
 }
